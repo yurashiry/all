91ecbd88bffbbd4f2f840e77bf26bf0439795d23
yurashiry <yurashiry@gmail.com>
2026-06-28 13:16:29 +0300

    Create verified.js

---
+++ id/js/verified.js
@@ -0,0 +1,20 @@
+// ==========================================
+// WR ID AT
+// Verified Users
+// ==========================================
+
+const verifiedUsers = {
+
+    // Основатель
+
+    "yurskk": {
+        blue: true,
+        plus: true
+    },
+    // Команда
+    "wrteam": {
+        blue: true,
+        plus: false
+    }
+
+};
